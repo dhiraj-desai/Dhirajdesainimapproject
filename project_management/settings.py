24dd1b4c06432a854a8523b1b64e483dc8825367
dhiraj-desai <131889248+dhiraj-desai@users.noreply.github.com>
2024-11-19 10:57:50 +0530

    Update settings.py

--- project_management/settings.py
+++ project_management/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # Add this line
+    # "whitenoise.middleware.WhiteNoiseMiddleware",  # Add this line
 ]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
